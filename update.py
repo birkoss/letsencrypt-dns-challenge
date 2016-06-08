abdd8be978e2228a4500639ada3af66aa963eef8
= <admin@birkoss.com>
2016-06-08 15:56:32 +0000

    Adding a delay in the script to allow the DNS server to refresh the new entry

--- update.py
+++ update.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python
+
 import sys
 import os
+import time
+
+if len(sys.argv) < 5:
+	exit(0)
 
 if sys.argv[1] == "deploy_challenge":
 	os.system("python3 ~/Code/cPanel-Update-Dns/updatedns.py --domain birkoss.com --type TXT --name _acme-challenge." + sys.argv[2] + ". --value '" + sys.argv[4] + "'")
-elif sys.argv[1] == "clean_challenge":
-	os.system("python3 ~/Code/cPanel-Update-Dns/updatedns.py --domain birkoss.com --type TXT --name _acme-challenge." + sys.argv[2] + ". --value ''")
+	print("   - Waiting 2 minutes to be sure the DNS are refreshed!")
+	time.sleep(120)
+#elif sys.argv[1] == "clean_challenge":
+#os.system("python3 ~/Code/cPanel-Update-Dns/updatedns.py --domain birkoss.com --type TXT --name _acme-challenge." + sys.argv[2] + ". --value ''")
